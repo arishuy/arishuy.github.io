342d8cb652241d6c68bcf554a4cba434a764e07e
Huy <88521078+arishuy@users.noreply.github.com>
2022-07-11 10:54:06 +0700

    Update

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "huyen" && password == "27112021"){
+    alert ("Login successfully");
+    window.location.href = "index.html";
+    }
+    else {
+        alert ("Login failed");
+    }
+    return false;
+}
---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.onload=function() {
+    // Month,Day,Year,Hour,Minute,Second
+    downTime('nov,27,2021,00:00:00');
+  }
+  function downTime(targetDate) {
+    now = new Date();
+    then = new Date(targetDate);
+     difference = (then-now);
+    days=Math.abs(Math.floor(difference/(60*60*1000*24)*1));
+    hours=Math.abs(Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1));
+    mins=Math.abs(Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1));
+    secs=Math.abs(Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1));
+    document.getElementById('daynumber').innerHTML = days;
+    document.getElementById('hoursnumber').innerHTML = hours;
+    document.getElementById('minutesnumber').innerHTML = mins;
+    document.getElementById('secsnumber').innerHTML = secs;
+    clearTimeout(downTime.to);
+    downTime.to=setTimeout(function(){ downTime(targetDate); },1000);
+  }
